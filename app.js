08ac551f431e59df33c5ddb633a3349fc611a09d
zsk-GitHub for Mac <zsk526@qq.com>
2015-09-30 00:36:21 +0800

    init

---
+++ app.js
@@ -0,0 +1,12 @@
+var http = require('http');
+
+http.createServer(function(req, res){
+	res.writeHead(200, {'Content-Type' : 'text/html'});
+	res.write('<h1>Node.js</h1>');
+	res.end('<p>Hello World !</p>');
+}).listen(3000);
+
+console.log('HTTp server is listening at port 3000.');
+
+
+
